49a2e58678f56e89049e0b75fb7f924c720dcef1
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-09-15 21:14:41 +0200

    add rx-for

--- src/app/components/dumb/movie-list/movie-list.component.ts
+++ src/app/components/dumb/movie-list/movie-list.component.ts
@@ -1,12 +1,12 @@
 import {Component, input, ChangeDetectionStrategy, output} from '@angular/core';
-import {NgFor} from '@angular/common';
 import {MovieListItemComponent} from "../movie-list-item/movie-list-item.component";
 import {Movie} from "../../../types/movie.types";
+import {RxFor} from "@rx-angular/template/for";
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [NgFor, MovieListItemComponent],
+  imports: [RxFor, MovieListItemComponent],
   templateUrl: './movie-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
